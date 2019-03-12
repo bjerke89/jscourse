89983542bfa5a26a6c174d1068b7163a59ec6582
nicholas-bjerke <nicholas.bjerke@uke.oslo.kommune.no>
2019-03-12 14:29:54 +0100

    finished lesson 32

--- Udemy/2-JS-basics/starter/script.js
+++ Udemy/2-JS-basics/starter/script.js
@@ -324,8 +324,8 @@ console.log(isDesigner);
 */
 
 //Challenge
-
-var tipArray = new Array;
+/*
+var tipArrayay = new Array;
 var billArray = [124,48,268];
 
 function calculcateTip(bill) {
@@ -356,36 +356,241 @@ function calculcateTip(bill) {
             default:
                 break;
         }
-        console.log(tipArray)
+        console.log(tipArrayay)
     }
 
 
     billArray.forEach(element => {
         var tip = calculcateTip(element);
-        tipArray.push(tip)
+        tipArrayay.push(tip)
     });
 
-console.log(tipArray);
+console.log(tipArrayay);
 
 
 var billArray = [124,48,268];
-var tipArray  = [calculcateTip(billArray[0]),
+var tipArrayay  = [calculcateTip(billArray[0]),
                  calculcateTip(billArray[1]),
                  calculcateTip(billArray[2])]
 
-var totalArray = [billArray[0] + calculcateTip(billArray[0]),
+var totalArrayay = [billArray[0] + calculcateTip(billArray[0]),
                   billArray[1] + calculcateTip(billArray[1]),
                   billArray[2] + calculcateTip(billArray[2])]
 
-console.log(tipArray)
-//console.log(totalArray)
+console.log(tipArrayay)
+//console.log(totalArrayay)
+*/
 
+//Objects and properties
 
+//Object literal
+/*
+var john = {
+    firstName: 'John', 
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher', 
+    isMarried: false
+};
 
+console.log(john.firstName);
+console.log(john['lastName']);
 
+var x = 'birthYear';
+console.log(john[x]);
 
+john.job = 'designer';
+john['isMarried'] = true;
 
+console.log(john);
 
+//New Object
+var jane = new Object();
+jane.name = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Doe';
+
+console.log(jane)
+*/
+//Objects and methods
+
+/*
+var john = {
+    firstName: 'John', 
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher', 
+    isMarried: false,
+    calcAge: function () {
+        this.age = 2019 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
+*/
+
+//Challenge 4
+/*
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    mass: 78,
+    height: 1.80,
+    calcBMI: function () {
+        this.BMI = this.mass / (this.height * this.height);
+        return this.BMI;
+    }
+}
+
+var mark = {
+    firstName:'Mark',
+    lastName: 'Johnson',
+    mass: 78,
+    height: 1.80,
+    calcBMI: function () {
+        this.BMI = this.mass / (this.height * this.height);
+        return this.BMI;
+    }
+}
+
+console.log(john,mark);
+
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log('John has the highest BMI with ' + john.BMI)
+} else if(mark.BMI > john.BMI) {
+    console.log('Mark has the highest BMi with ' + mark.BMI)
+} else {
+    console.log('The BMI are the same. BMI John: ' + john.BMI + ' BMI Mark: ' + mark.BMI)
+}
+
+*/
+
+//Loops
+/*
+for (let index = 0; index < 10; index++) {
+    console.log(index)    
+}
+for (let index = 1; index <= 10; index += 2) {
+    console.log(index)    
+}
+*/
+/*
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+// for loop
+for (let index = 0; index < john.length; index++) {
+    console.log(john[index]);   
+}
+
+//while loop
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+//continue break
+
+/*
+var john = ['John', 'Smith', 1990, 'designer', false];
+/*
+for (let index = 0; index < john.length; index++) {
+    if (typeof john[index] !== 'string') continue;
+    console.log(john[index]);   
+}
+
+for (let index = 0; index < john.length; index++) {
+    if (typeof john[index] !== 'string') break;
+    console.log(john[index]);   
+}
+*/
+
+//Loop reverse
+/*
+for (let index = john.length - 1; index >= 0; index--) {
+    console.log(john[index]);   
+}
+*/
+
+//Challenge 5
+
+var billsJohn = {
+    billsJohnArray: [124, 48, 268, 180, 42],
+    tipArray: new Array,
+    totalArray: new Array,
+    calcTip: function() {
+        for (let index = 0; index < this.billsJohnArray.length; index++) {
+            if (this.billsJohnArray[index] < 50) {
+                this.tip = (this.billsJohnArray[index] * 20) / 100;
+                this.tipArray.push(this.tip);
+                this.totalArray.push(this.billsJohnArray[index] + this.tip);
+            } else if (this.billsJohnArray[index] > 50 && this.billsJohnArray[index] < 200) {
+                this.tip = (this.billsJohnArray[index] * 15) / 100;
+                this.tipArray.push(this.tip);
+                this.totalArray.push(this.billsJohnArray[index] + this.tip);
+            } else {
+                this.tip = (this.billsJohnArray[index] * 10) / 100;
+                this.tipArray.push(this.tip);
+                this.totalArray.push(this.billsJohnArray[index] + this.tip);
+            }
+        }
+    }
+}
+
+billsJohn.calcTip();
+
+console.log(billsJohn.tipArray, billsJohn.totalArray)
+
+var billsMark = {
+    billsMarkArray: [77, 475, 110, 45],
+    tipArray: new Array,
+    totalArray: new Array,
+    calcTip: function() {
+        for (let index = 0; index < this.billsMarkArray.length; index++) {
+            if (this.billsMarkArray[index] < 100) {
+                this.tip = (this.billsMarkArray[index] * 20) / 100;
+                this.tipArray.push(this.tip);
+                this.totalArray.push(this.billsMarkArray[index] + this.tip);
+            } else if (this.billsMarkArray[index] > 100 && this.billsMarkArray[index] < 300) {
+                this.tip = (this.billsMarkArray[index] * 10) / 100;
+                this.tipArray.push(this.tip);
+                this.totalArray.push(this.billsMarkArray[index] + this.tip);
+            } else {
+                this.tip = (this.billsMarkArray[index] * 25) / 100;
+                this.tipArray.push(this.tip);
+                this.totalArray.push(this.billsMarkArray[index] + this.tip);
+            }
+        }
+    }
+}
+
+billsMark.calcTip();
+
+console.log(billsMark.tipArray, billsMark.totalArray)
+
+var averageTips = function(tips) {
+    var sum = 0;
+    for (let index = 0; index <tips.length; index++) {
+        sum = sum + tips[index]
+    }
+    return sum / tips.length;
+}
+
+johnAverage = averageTips(billsJohn.tipArray);
+markAverage = averageTips(billsMark.tipArray);
+
+console.log(johnAverage, markAverage);
+
+if (johnAverage > markAverage) {
+    console.log('John\'s family pays higher tips with an average of ' + johnAverage);
+} else if(markAverage > johnAverage) {
+    console.log('Mark\'s family pays higher tips with an average of ' + markAverage)
+} else {
+    console.log('Both family pays the same amount of tips on average');
+}
 
 
 
