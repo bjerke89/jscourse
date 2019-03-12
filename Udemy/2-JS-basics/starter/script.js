2e45e95d485fd4f8e4193d284f9a4ec69c4307fc
Nicholas Bjerke <bjerke89@me.com>
2019-03-12 09:38:54 +0100

    Lesson 19 complete

--- Udemy/2-JS-basics/starter/script.js
+++ Udemy/2-JS-basics/starter/script.js
@@ -53,7 +53,7 @@ console.log(typeof johnOlder);
 */
 
 //Operator precedence
-
+/*
 var now = 2019;
 var bornJohn = 1989;
 var fullAge = 18;
@@ -83,8 +83,9 @@ x++;
 console.log(x);
 
 //Challenge 1 
-
+/*
 var markMass = 85;
+
 var johnMass = 82;
 var markHeight = 1.85;
 var johnHeight = 1.95;
@@ -98,4 +99,136 @@ if (johnBmi > markBmi) {
     console.log('John\'s BMI is higher with ' + johnBmi)
 } else {
     console.log('Mark\'s BMI is higher with ' + markBmi)
-};
+}; */
+
+//If else
+
+/*
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!')
+} else {
+    console.log(firstName + ' is not married!')
+}
+
+
+var isMarried = true;
+if (civilStatus) {
+    console.log(firstName + ' is married!')
+} else {
+    console.log(firstName + ' is not married!')
+}
+
+//Boolean logic
+
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy')
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager')
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man')
+} else {
+    console.log(firstName + ' is a min')
+}
+
+*/
+
+//ternery operator
+
+var firstName = 'John';
+/*
+var age = 22;
+
+age >= 18 ? console.log(firstName + ' drinks beer') :
+    console.log(firstName + ' drinks juice');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+*/
+//switch
+/*
+var job = 'instructor';
+
+
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids');
+        break;
+    case 'driver':
+        console.log(firstName + ' is a driver');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs cars');
+        break;
+    default:
+        console.log(firstName + ' has a no job')
+        break;
+}
+
+var age = 32;
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man');
+        break;
+    default:
+        console.log(firstName + ' is a man');
+        break;
+}
+*/
+
+//truthy false values
+
+//falsy values: undefined, null, 0 '', NaN.
+//truthy values: not falsy
+/*
+var height;
+
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable is not defined');
+}
+
+//equality operators
+
+if (height === '23') {
+    console.log('The == operator does type coercion!');
+} else {
+    console.log('The === operator does strict type coercion')
+} */
+
+
+//Challenge 2
+
+var teamJohnAvgScore = (89 + 120 + 103) / 3;
+var teamMikeAvgScore = (116 + 94 + 123) / 3;
+var teamMaryAvgScore = (97 + 134 + 105) / 3;
+
+console.log(teamJohnAvgScore, teamMikeAvgScore, teamMaryAvgScore);
+
+
+
+if (teamJohnAvgScore > teamMikeAvgScore && teamJohnAvgScore > teamMaryAvgScore) {
+    console.log('John\'s team scored a higher average with ' + teamJohnAvgScore);
+} else if (teamMikeAvgScore > teamJohnAvgScore && teamMikeAvgScore > teamMaryAvgScore && teamMikeAvgScore === teamMaryAvgScore) {
+    console.log('Mike\'s team scored a higher average with ' + teamMikeAvgScore);
+} else if (teamMaryAvgScore > teamJohnAvgScore && teamMikeAvgScore) {
+    console.log('Mary\'s team scored a higher average with ' + teamMaryAvgScore);
+} else {
+    console.log('The scores are equal and it is a tie');
+}
