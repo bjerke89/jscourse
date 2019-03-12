b04bc9d91c9136108650142d4a43fc4c30c3df1a
nicholas-bjerke <nicholas.bjerke@uke.oslo.kommune.no>
2019-03-12 11:34:09 +0100

    Lesson 25 complete

--- Udemy/2-JS-basics/starter/script.js
+++ Udemy/2-JS-basics/starter/script.js
@@ -140,7 +140,7 @@ if (age < 13) {
 
 //ternery operator
 
-var firstName = 'John';
+//var firstName = 'John';
 /*
 var age = 22;
 
@@ -214,7 +214,7 @@ if (height === '23') {
 
 
 //Challenge 2
-
+/*
 var teamJohnAvgScore = (89 + 120 + 103) / 3;
 var teamMikeAvgScore = (116 + 94 + 123) / 3;
 var teamMaryAvgScore = (97 + 134 + 105) / 3;
@@ -232,3 +232,163 @@ if (teamJohnAvgScore > teamMikeAvgScore && teamJohnAvgScore > teamMaryAvgScore) 
 } else {
     console.log('The scores are equal and it is a tie');
 }
+
+*/
+
+// Functions
+
+/*
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0 ) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName +  ' is already retired');
+    }
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
+*/
+
+// function statements expressions
+
+//func declaration
+//function whatDoYouDo(job, firstName) {}
+
+/*func expression
+var whatDoYouDo = function(job, firstName) {
+    switch (job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+    
+        case 'driver':
+            return firstName + ' drives uber';
+
+        case 'designer':
+            return firstName + ' designs clothes';
+        
+        default:
+            return firstName + ' does not have a valid job';
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('retired', 'Mark'));
+console.log(whatDoYouDo('designer', 'Jane'));
+*/
+
+//Arrays
+/*
+var names = ['John', 'Mike', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names);
+console.log(names.length);
+
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+
+console.log(names);
+
+//diff types
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+
+console.log(john);
+
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1923));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is not a designer' : 'John is a designer';
+console.log(isDesigner);
+
+*/
+
+//Challenge
+
+var tipArray = new Array;
+var billArray = [124,48,268];
+
+function calculcateTip(bill) {
+    
+    if (bill < 50) {
+        tipPercentage = .20;
+
+    } else if(bill > 50 && bill < 200) {
+        tipPercentage = .15;
+    } else {
+        tipPercentage = .1;
+    }
+        return tipPercentage * bill
+}
+
+
+    function tipCalc(bill) {
+        switch (true) {
+            case (bill < 50):
+                return (bill * 20) / 100
+
+            case (bill> 50 && bill < 200):
+                return (bill * 15) / 100;
+            
+            case (bill > 200):
+                return (bill * 10) / 100;
+            
+            default:
+                break;
+        }
+        console.log(tipArray)
+    }
+
+
+    billArray.forEach(element => {
+        var tip = calculcateTip(element);
+        tipArray.push(tip)
+    });
+
+console.log(tipArray);
+
+
+var billArray = [124,48,268];
+var tipArray  = [calculcateTip(billArray[0]),
+                 calculcateTip(billArray[1]),
+                 calculcateTip(billArray[2])]
+
+var totalArray = [billArray[0] + calculcateTip(billArray[0]),
+                  billArray[1] + calculcateTip(billArray[1]),
+                  billArray[2] + calculcateTip(billArray[2])]
+
+console.log(tipArray)
+//console.log(totalArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
