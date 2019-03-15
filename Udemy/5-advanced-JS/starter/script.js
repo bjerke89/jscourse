f32dca32f44e23f2d2b04c641a26e9a585053d86
nicholas-bjerke <nicholas.bjerke@uke.oslo.kommune.no>
2019-03-15 14:00:33 +0100

    finished lesson 62

---
+++ Udemy/5-advanced-JS/starter/script.js
@@ -0,0 +1,32 @@
+
+
+// function constructor
+
+/*var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+}*/
+
+var Person = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function () {
+    console.log(2019 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1968, 'designer');
+var mark = new Person('Mark', 1974, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName, jane.lastName, mark.lastName);
